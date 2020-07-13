80065a689d4788f2423a4e68825d51e50346b389
Juan Pujol <joangalilea@gmail.com>
2020-07-13 18:39:07 +0200

    Add media queries

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const shareIcon = document.querySelector(".share-icon-box");
 const shareTab = document.querySelector(".share-tab");
+const contentFooter  = document.querySelector(".content__footer");
 
 shareIcon.addEventListener("click", () => {
+    if(screen.width <= 900) {
+        contentFooter.style.backgroundColor = "#48556a";
+    }
+
     shareTab.classList.toggle("active");
 });
